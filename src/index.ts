8d625945e0b9b2059437e3dfac12733f3286c5a3
Dzevad <dzevad.del@gmail.com>
2022-03-19 12:35:16 +0100

    finished mobile design

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
-function clampBuilder(
-  minWidthPx,
-  maxWidthPx,
-  minFontSize,
-  maxFontSize,
-  pixelsPerRem
-) {
-  const minWidth = minWidthPx / pixelsPerRem;
-  const maxWidth = maxWidthPx / pixelsPerRem;
 
-  const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
-  const yAxisIntersection = -minWidth * slope + minFontSize;
-
-  return `clamp( ${minFontSize}rem, ${parseFloat(
-    yAxisIntersection.toFixed(4)
-  )}rem + ${parseFloat((slope * 100).toFixed(4))}vw, ${maxFontSize}rem )`;
-}
-
-console.log(clampBuilder(375, 800, 1.3, 1.6, 10));
