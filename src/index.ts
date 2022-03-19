877cc82ca95e16c9e1263da5f41f0dc272f5f775
Dzevad <dzevad.del@gmail.com>
2022-03-19 21:47:31 +0100

    finished desktop styles and form validation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import emailValidator from 'email-validator';
+
 // get all input fields
 const firstName = document.querySelector('.first-name')! as HTMLInputElement;
 const lastName = document.querySelector('.last-name')! as HTMLInputElement;
@@ -8,44 +10,129 @@ const password = document.querySelector('.password')! as HTMLInputElement;
 const signupForm = document.querySelector('.signup__form')! as HTMLFormElement;
 
 // get all errors
+const fieldFirstName = document.querySelector(
+  '.field-box--first-name'
+)! as HTMLDivElement;
+
+const fieldLastName = document.querySelector(
+  '.field-box--last-name'
+)! as HTMLDivElement;
+
+const fieldEmail = document.querySelector(
+  '.field-box--email'
+)! as HTMLDivElement;
+
+const fieldPassword = document.querySelector(
+  '.field-box--password'
+)! as HTMLDivElement;
 
 interface SignupForm {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
+  firstName: Record<string, string>;
+  lastName: Record<string, string>;
+  email: Record<string, string>;
+  password: Record<string, string>;
 }
 
 const signupFormFields: SignupForm = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  password: '',
+  firstName: {
+    value: '',
+    error: 'First name cannot be empty',
+    type: 'text',
+  },
+  lastName: {
+    value: '',
+    error: 'Last name cannot be empty',
+    type: 'text',
+  },
+  email: {
+    value: '',
+    error: 'Looks like this is not an email',
+    type: 'email',
+  },
+  password: {
+    value: '',
+    error: 'Password cannot be empty',
+    type: 'password',
+  },
 };
 
 // add event listeners
 firstName.addEventListener(
   'change',
-  (e) => (signupFormFields['firstName'] = (e.target as HTMLInputElement).value)
+  (e) =>
+    (signupFormFields['firstName']['value'] = (
+      e.target as HTMLInputElement
+    ).value)
 );
 
 lastName.addEventListener(
   'change',
-  (e) => (signupFormFields['lastName'] = (e.target as HTMLInputElement).value)
+  (e) =>
+    (signupFormFields['lastName']['value'] = (
+      e.target as HTMLInputElement
+    ).value)
 );
 
 email.addEventListener(
   'change',
-  (e) => (signupFormFields['email'] = (e.target as HTMLInputElement).value)
+  (e) =>
+    (signupFormFields['email']['value'] = (e.target as HTMLInputElement).value)
 );
 
 password.addEventListener(
   'change',
-  (e) => (signupFormFields['password'] = (e.target as HTMLInputElement).value)
+  (e) =>
+    (signupFormFields['password']['value'] = (
+      e.target as HTMLInputElement
+    ).value)
 );
 
 // handle signup form
 signupForm.addEventListener('submit', (e): void => {
   e.preventDefault();
-  console.log(signupFormFields);
+
+  handleField(fieldFirstName, signupFormFields, 'firstName');
+  handleField(fieldLastName, signupFormFields, 'lastName');
+  handleField(fieldPassword, signupFormFields, 'password');
+  handleField(fieldEmail, signupFormFields, 'email');
 });
+
+const handleField = (
+  field: HTMLDivElement,
+  obj: SignupForm,
+  key: string
+): void => {
+  if (obj[key].type === 'email') {
+    // input type email
+    if (!emailValidator.validate(obj[key]['value'])) {
+      field.lastElementChild.innerHTML = obj[key]['error'];
+      field.firstElementChild.classList.add('error-field');
+
+      const [input, icon] = field.firstElementChild.children;
+      icon.classList.add('error-icon');
+      input.classList.add('error-input');
+    } else {
+      field.lastElementChild.innerHTML = '';
+      field.firstElementChild.classList.remove('error-field');
+
+      const [input, icon] = field.firstElementChild.children;
+      icon.classList.remove('error-icon');
+      input.classList.remove('error-input');
+    }
+  } else {
+    // input type text
+    if (obj[key]['value'] === '') {
+      field.lastElementChild.innerHTML = obj[key]['error'];
+      field.firstElementChild.classList.add('error-field');
+
+      const [, icon] = field.firstElementChild.children;
+      icon.classList.add('error-icon');
+    } else {
+      field.lastElementChild.innerHTML = '';
+      field.firstElementChild.classList.remove('error-field');
+
+      const [, icon] = field.firstElementChild.children;
+      icon.classList.remove('error-icon');
+    }
+  }
+};
